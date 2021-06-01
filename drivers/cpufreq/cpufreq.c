d911ff741c0a9336ac2ccd9005c3e6b695b08778
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-06-01 19:52:35 +0200

    thermal: core: Implement xiaomi mi thermald interface
    Reverse Engineered from MIUI CN 13.0.7.0
    
    Change-Id: I8842b811e3e2dfcb5f82eadea8fe72392c43e544
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2270,9 +2270,9 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 			CPUFREQ_ADJUST, new_policy);
 
 #ifndef CONFIG_BOARD_XIAOMI
- 	/* adjust if necessary - hardware incompatibility */
- 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
- 			CPUFREQ_INCOMPATIBLE, new_policy);
+	/* adjust if necessary - hardware incompatibility */
+	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
+			CPUFREQ_INCOMPATIBLE, new_policy);
 #else
 	/* the adjusted frequency should not exceed thermal limit*/
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
