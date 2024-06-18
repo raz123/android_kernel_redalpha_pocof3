e72d5574d4535b38efc404a692775def690dcada
UtsavBalar1231 <utsavbalar1231@gmail.com>
2024-06-18 15:50:51 +0530

    serial: msm_geni_serial: Ensure IRQ is properly managed during startup and shutdown
    Change-Id: I395df5249a873586f7a437e32f638b9d79bd2093
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -2445,11 +2445,8 @@ static void msm_geni_serial_shutdown(struct uart_port *uport)
 		msm_geni_serial_stop_tx(uport);
 	}
 
-#if !defined(CONFIG_BOARD_ELISH) && !defined(CONFIG_BOARD_ENUMA) && \
-	!defined(CONFIG_BOARD_DAGU) && !defined(CONFIG_BOARD_PIPA)
 	if (likely(!uart_console(uport)))
 		disable_irq(uport->irq);
-#endif
 
 	if (!uart_console(uport)) {
 		if (msm_port->ioctl_count) {
@@ -2590,12 +2587,8 @@ static int msm_geni_serial_startup(struct uart_port *uport)
 		}
 	}
 
-#if !defined(CONFIG_BOARD_ELISH) && !defined(CONFIG_BOARD_ENUMA) && \
-	!defined(CONFIG_BOARD_DAGU) && !defined(CONFIG_BOARD_PIPA)
 	if (likely(!uart_console(uport)))
 		enable_irq(uport->irq);
-#endif
-
 	/*
 	 * Ensure that all the port configuration writes complete
 	 * before returning to the framework.
@@ -3666,9 +3659,6 @@ static int msm_geni_serial_probe(struct platform_device *pdev)
 	irq_set_status_flags(uport->irq, IRQ_NOAUTOEN);
 	ret = devm_request_irq(uport->dev, uport->irq, msm_geni_serial_isr,
 				IRQF_TRIGGER_HIGH, dev_port->name, uport);
-
-#if !defined(CONFIG_BOARD_ELISH) && !defined(CONFIG_BOARD_ENUMA) && \
-	!defined(CONFIG_BOARD_DAGU) && !defined(CONFIG_BOARD_PIPA)
 	if (likely(!uart_console(uport))) {
 		/*
 		 * irq should disable untill msm_geni_serial_port_setup
@@ -3676,8 +3666,6 @@ static int msm_geni_serial_probe(struct platform_device *pdev)
 		 */
 		disable_irq(uport->irq);
 	}
-#endif
-
 	if (ret) {
 		dev_err(uport->dev, "%s: Failed to get IRQ ret %d\n",
 							__func__, ret);
