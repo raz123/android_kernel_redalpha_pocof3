c57dd8616e45c76d03e79d8001a593f4c882507b
raz123 <redag2@gmail.com>
2026-06-22 12:32:29 -0400

    arm64: alioth: fix ksu_handle_vfs_fstat guard to CONFIG_KSU_SUSFS
    ksu_handle_vfs_fstat is SUSFS-only. Revert its guard and extern
    declaration to CONFIG_KSU_SUSFS with original SUSFS symbol names.

--- fs/stat.c
+++ fs/stat.c
@@ -151,8 +151,11 @@ EXPORT_SYMBOL(vfs_getattr);
  */
 #ifdef CONFIG_KSU_MANUAL_HOOK
 extern struct static_key_true ksu_init_rc_hook;
+#endif
+#ifdef CONFIG_KSU_SUSFS
+extern struct static_key_true ksu_is_init_rc_hook_enabled;
 extern void ksu_handle_vfs_fstat(int fd, loff_t *kstat_size_ptr);
-#endif // #ifdef CONFIG_KSU_MANUAL_HOOK
+#endif
 
 int vfs_statx_fd(unsigned int fd, struct kstat *stat,
 		 u32 request_mask, unsigned int query_flags)
@@ -167,10 +170,10 @@ int vfs_statx_fd(unsigned int fd, struct kstat *stat,
 	if (f.file) {
 		error = vfs_getattr(&f.file->f_path, stat,
 				    request_mask, query_flags);
-#ifdef CONFIG_KSU_MANUAL_HOOK
-		if (static_branch_unlikely(&ksu_init_rc_hook))
+#ifdef CONFIG_KSU_SUSFS
+		if (static_branch_unlikely(&ksu_is_init_rc_hook_enabled))
 			ksu_handle_vfs_fstat(fd, &stat->size);
-#endif // #ifdef CONFIG_KSU_MANUAL_HOOK
+#endif
 
 		fdput(f);
 	}
