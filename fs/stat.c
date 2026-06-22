993f904e63e5aabef4f9534966b3316f25a380f5
raz123 <redag2@gmail.com>
2026-06-22 11:55:43 -0400

    arm64: alioth: fix KSU symbol names for MANUAL_HOOK mode

--- fs/stat.c
+++ fs/stat.c
@@ -150,7 +150,7 @@ EXPORT_SYMBOL(vfs_getattr);
  * 0 will be returned on success, and a -ve error code if unsuccessful.
  */
 #ifdef CONFIG_KSU_SUSFS
-extern struct static_key_true ksu_is_init_rc_hook_enabled;
+extern struct static_key_true ksu_init_rc_hook;
 extern void ksu_handle_vfs_fstat(int fd, loff_t *kstat_size_ptr);
 #endif // #ifdef CONFIG_KSU_SUSFS
 
@@ -168,7 +168,7 @@ int vfs_statx_fd(unsigned int fd, struct kstat *stat,
 		error = vfs_getattr(&f.file->f_path, stat,
 				    request_mask, query_flags);
 #ifdef CONFIG_KSU_SUSFS
-		if (static_branch_unlikely(&ksu_is_init_rc_hook_enabled))
+		if (static_branch_unlikely(&ksu_init_rc_hook))
 			ksu_handle_vfs_fstat(fd, &stat->size);
 #endif // #ifdef CONFIG_KSU_SUSFS
 
