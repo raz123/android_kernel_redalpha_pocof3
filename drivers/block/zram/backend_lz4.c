b690e87be7b32a241fef6fba8d94db52713e556b
raz123 <redag2@gmail.com>
2026-06-22 00:51:17 -0400

    redalpha: rewrite backend_lz4.c clean (remove broken debug prints)
    Debug prints from previous commit broke dictionary compression:
    - zctx->mem NULL check fired before dict/non-dict branch
    - In dict mode, zctx->mem is intentionally NULL
    
    Rewrote file clean with correct code. No debug prints.

--- drivers/block/zram/backend_lz4.c
+++ drivers/block/zram/backend_lz4.c
@@ -22,13 +22,13 @@
 #include <linux/vmalloc.h>
 
 #include "backend_lz4.h"
+
 #ifndef LZ4_ACCELERATION_DEFAULT
 #define LZ4_ACCELERATION_DEFAULT 1
 #endif
 
 struct lz4_ctx {
 	void *mem;
-
 	LZ4_streamDecode_t *dstrm;
 	LZ4_stream_t *cstrm;
 };
@@ -89,7 +89,6 @@ static void lz4_destroy(struct zcomp_ctx *ctx)
 static int lz4_create(struct zcomp_params *params, struct zcomp_ctx *ctx)
 {
 	struct lz4_ctx *zctx;
-	pr_err("lz4_create: params=%p, dict_sz=%zu\n", params, params ? params->dict_sz : 0);
 
 	zctx = kzalloc(sizeof(*zctx), GFP_KERNEL);
 	if (!zctx)
@@ -122,8 +121,6 @@ static int lz4_compress(struct zcomp_params *params, struct zcomp_ctx *ctx,
 {
 	struct lz4_ctx *zctx = ctx->context;
 	int ret;
-	if (!zctx) { pr_err("lz4_compress: zctx is NULL\n"); return -EINVAL; }
-	if (!zctx->mem) { pr_err("lz4_compress: zctx->mem is NULL\n"); return -EINVAL; }
 
 	if (!zctx->cstrm) {
 		ret = LZ4_compress_fast_extState(zctx->mem, req->src, req->dst,
@@ -147,7 +144,6 @@ static int lz4_decompress(struct zcomp_params *params, struct zcomp_ctx *ctx,
 {
 	struct lz4_ctx *zctx = ctx->context;
 	int ret;
-	if (!zctx) { pr_err("lz4_decompress: zctx is NULL\n"); return -EINVAL; }
 
 	if (!zctx->dstrm) {
 		ret = LZ4_decompress_safe(req->src, req->dst, req->src_len,
