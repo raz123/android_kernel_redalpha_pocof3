f138c8ab713e7032942cca2d69f1fd5ae92d40c4
EmanuelCN <emanuelghub@gmail.com>
2026-06-28 14:27:25 -0400

    cpufreq: schedutil: Store the cached ratelimits values

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1218,6 +1218,8 @@ static void sugov_tunables_save(struct cpufreq_policy *policy,
 	cached->down_rate_limit_us = tunables->down_rate_limit_us;
 }
 
+
+
 static void sugov_clear_global_tunables(void)
 {
 	if (!have_governor_per_policy())
@@ -1351,10 +1353,14 @@ static void sugov_exit(struct cpufreq_policy *policy)
 
 	count = gov_attr_set_put(&tunables->attr_set, &sg_policy->tunables_hook);
 	policy->governor_data = NULL;
+
+
 	if (!count) {
 		sugov_tunables_save(policy, tunables);
 		sugov_clear_global_tunables();
 	}
+
+
 
 	mutex_unlock(&global_tunables_lock);
 
